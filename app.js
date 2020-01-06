606c696c4f5a974a61865d02c985e11e5851fff0
Marek Potocek <MarekPotocek@seznam.cz>
2020-01-06 16:06:17 +0100

    Opravy kodu + alert

--- app.js
+++ app.js
@@ -33,10 +33,24 @@ class UI {
         <td>$[book.title}</td>
         <td>$[book.author}</td>
         <td>$[book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        <td><a href="#" class="btn btn-danger btn-sm
+         delete">X</a></td>
         `;
 
         list.appendChild(row)
+    }
+
+    static deleteBook(el) {
+    if(el.classList.contains('delete')){
+        el.parentElement.parentElement.remove();
+     }
+    }
+
+
+    static clearFields(){
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+        document.querySelector('#isbn').value = '';
     }
 }
 //zobraz knihy
@@ -47,12 +61,24 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     // Prevent actual submit
     e.preventDefault();
 
-    // Get form values
+    // Vybrání věci z formuláře
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
+    //ověření
+    if(title === '' || author === '' || isbn === ''){
+        alert('Prosím doplň data')
+    }
+
     const  book = new Book(title,author,isbn);
 
-    console.log(book)
+    UI.addBookToList(book);
+
+    UI.clearFields();
+});
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+    // Remove book from UI
+    UI.deleteBook(e.target);
 });
