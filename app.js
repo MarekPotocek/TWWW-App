d3999d5be6e138a46efe6c64e80f45ae6d840e85
Marek Potocek <MarekPotocek@seznam.cz>
2020-01-06 20:33:17 +0100

    Špatné závorky :))

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ class UI {
         const  row = document.createElement('tr');
 
         row.innerHTML = `
-        <td>$[book.title}</td>
-        <td>$[book.author}</td>
-        <td>$[book.isbn}</td>
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
         <td><a href="#" class="btn btn-danger btn-sm
          delete">X</a></td>
         `;
