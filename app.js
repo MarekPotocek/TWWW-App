967426b50ed484bbb7af3377f38576b9586da042
Marek Potocek <MarekPotocek@seznam.cz>
2020-01-06 15:50:16 +0100

    Fixing chybu v HTML + upgrade JS

--- app.js
+++ app.js
@@ -43,9 +43,16 @@ class UI {
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 //přidání knihy
-document.querySelector('#book-form').addEventListener('submit', (e)=> {
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+
+    // Get form values
     const title = document.querySelector('#title').value;
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
+    const  book = new Book(title,author,isbn);
+
+    console.log(book)
 });
