73614b154b0e1d7a9a584e9e77d3717b58added4
Marek Potocek <MarekPotocek@seznam.cz>
2020-01-06 15:33:36 +0100

    Vytváření tabulky po přídání knihy pomocí JS

--- app.js
+++ app.js
@@ -21,5 +21,23 @@ class UI {
             }
         ];
         const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const  row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>$[book.title}</td>
+        <td>$[book.author}</td>
+        <td>$[book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row)
     }
 }
+//zobraz knihy
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
